1f185527d3e3c68554ef5f56d049d60da63bff5b
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-05 12:55:36 +0530

    Time: 7 ms (80.38%), Space: 52.2 MB (82.18%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    //cycle detection code
+    bool dfs(vector<vector<int>>& graph,int node, vector<int>& vis,vector<int>&visPath){
+        vis[node] = 1;
+        visPath[node] = 1;
+        for(auto& it:graph[node]){
+            if(!vis[it] && dfs(graph,it,vis,visPath))return true;
+            else if(visPath[it]) return true;
+        }
+        visPath[node]=0;
+        return false;
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        
+        vector<int>res,vis(n,0),visPath(n,0);
+
+        for(int i=0;i<n;i++){
+            dfs(graph,i,vis,visPath);
+        }
+        for(int i =0;i<n;i++) if(visPath[i]!=1) res.push_back(i);
+        return res;
+    }
+};
