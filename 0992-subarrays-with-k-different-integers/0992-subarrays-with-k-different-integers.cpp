4377dbf1dea3ec911eb0b084e33884ecbbef83c0
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-10 10:18:24 +0530

    Time: 98 ms (20.24%), Space: 56.2 MB (39.15%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,24 +1,22 @@
 class Solution {
-public:
-    int calcK(vector<int>& nums, int k){
-        if(k<0) return 0;
-        int n = nums.size();
-        int left=0,count=0;
-        unordered_map<int,int> mp;
-        for(int right=0;right<n;right++){
-            mp[nums[right]]++;
-            if(mp.size()>k){
-                while(mp.size()>k){
-                    mp[nums[left]]--;
-                    if(mp[nums[left]]==0) mp.erase(nums[left]);
-                    left++;
-                }
+    int solve(vector<int>&nums,int k){
+        unordered_map<int,int>mp;
+        int left = 0;
+        int count = 0;
+        for(int right = 0; right<nums.size(); right++){
+            int curr = nums[right];
+            mp[curr]++;
+            while(mp.size()>k){
+                mp[nums[left]]--;
+                if(mp[nums[left]]==0) mp.erase(nums[left]);
+                left++;
             }
-            count += right-left+1;
+            count+=right-left+1;
         }
         return count;
     }
+public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return calcK(nums,k)- calcK(nums,k-1);
+        return solve(nums,k) - solve(nums,k-1);
     }
 };
