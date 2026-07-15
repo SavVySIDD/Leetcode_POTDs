258e8c6aff150c8e6c4ee764db02319742c3071d
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-16 00:26:45 +0530

    Time: 5 ms (33.01%), Space: 37.2 MB (82.4%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,76 @@
+class Solution {
+
+    class BSTIterator {
+
+        stack<TreeNode*> st;
+        bool reverse;
+
+        void pushAll(TreeNode* root){
+
+            while(root){
+
+                st.push(root);
+
+                if(reverse)
+                    root = root->right;
+                else
+                    root = root->left;
+            }
+        }
+
+    public:
+
+        BSTIterator(TreeNode* root, bool rev){
+
+            reverse = rev;
+            pushAll(root);
+        }
+
+        int next(){
+
+            TreeNode* temp = st.top();
+            st.pop();
+
+            if(reverse)
+                pushAll(temp->left);
+            else
+                pushAll(temp->right);
+
+            return temp->val;
+        }
+
+        bool hasNext(){
+
+            return !st.empty();
+        }
+    };
+
+public:
+
+    bool findTarget(TreeNode* root, int k) {
+
+        if(!root) return false;
+
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int left = l.next();
+        int right = r.next();
+
+        while(left < right){
+
+            int sum = left + right;
+
+            if(sum == k)
+                return true;
+
+            else if(sum < k)
+                left = l.next();
+
+            else
+                right = r.next();
+        }
+
+        return false;
+    }
+};
