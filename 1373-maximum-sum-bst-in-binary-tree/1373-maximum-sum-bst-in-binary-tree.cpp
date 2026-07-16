3d4415e3774fa4d5282c331f93a3ccd168f85ded
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-16 16:45:46 +0530

    Time: 10 ms (42.6%), Space: 104.3 MB (76.39%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,69 @@
+class Solution {
+
+    struct NodeInfo{
+
+        bool isBST;
+
+        int sum;
+
+        int minVal;
+
+        int maxVal;
+    };
+
+    int ans = 0;
+
+    NodeInfo solve(TreeNode* root){
+
+        if(!root){
+
+            return {true, 0, INT_MAX, INT_MIN};
+        }
+
+        NodeInfo left = solve(root->left);
+
+        NodeInfo right = solve(root->right);
+
+        if(left.isBST &&
+           right.isBST &&
+           root->val > left.maxVal &&
+           root->val < right.minVal){
+
+            int currSum = left.sum + right.sum + root->val;
+
+            ans = max(ans, currSum);
+
+            return {
+
+                true,
+
+                currSum,
+
+                min(root->val, left.minVal),
+
+                max(root->val, right.maxVal)
+
+            };
+        }
+
+        return {
+
+            false,
+
+            0,
+
+            INT_MIN,
+
+            INT_MAX
+        };
+    }
+
+public:
+
+    int maxSumBST(TreeNode* root) {
+
+        solve(root);
+
+        return ans;
+    }
+};
