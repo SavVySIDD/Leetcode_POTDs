bd98ff2ae7bcaf9dcc5b7e586891c6a0e844ff6f
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-19 12:26:53 +0530

    Time: 7 ms (45.04%), Space: 11.1 MB (19.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int left =0, maxF=0;
-        int count =0;
-        vector <int> hash(26,0);
-        for(int right=0; right < n; right++){
-            hash[s[right]-'A']++;
-            maxF = max(maxF, hash[s[right]-'A']);
-            if((right-left+1)-maxF > k){
-                hash[s[left]-'A']--; 
+        int left =0,maxF=INT_MIN;
+        unordered_map<char,int>mp;
+        int res = INT_MIN;
+        for(int right =0;right<n;right++){
+            char curr = s[right];
+            mp[curr]++;
+            maxF = max(maxF,mp[curr]);
+            if(right-left+1>maxF+k){
+                mp[s[left]]--;
                 maxF=0;
-                for(int i=0;i<26;i++) maxF = max(maxF, hash[i]);
+                for(auto&it:mp) maxF = max(maxF,it.second);
                 left++;
             }
-            count = max(count, right-left+1);
+            res = max(res,right-left+1);
         }
-        return count;
+        return res;
     }
 };
