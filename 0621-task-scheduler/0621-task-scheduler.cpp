90358e103b6615f85aea44d4996b54db2b12ccbe
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-19 23:09:16 +0530

    Time: 39 ms (31.51%), Space: 47.7 MB (32.45%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -7,36 +7,29 @@ public:
         for(char &ch : tasks) {
             mp[ch]++;
         }
-
-        priority_queue<int> pq; //max heap
-        //we want to finish the process which is most occurring (having highest frequency)
-        //so that we don't have to finish in the last with p gaps.
+        priority_queue<int> pq; //max 
         int time = 0;
-        
         for(auto &it : mp) {
             pq.push(it.second);
         }
-        
         while(!pq.empty()) {
             vector<int> temp;
-            for(int i = 1; i<=p+1; i++) {
-                //filling first p+1 characters
+            for(int i = 1; i<=p+1; i++) { //p+1 because we have to wait for p times after performing the same task.
                 if(!pq.empty()) {
-                    temp.push_back(pq.top()-1); //finishing one instance of each process
+                    int freq = pq.top();
+                    freq--;
+                    temp.push_back(freq); 
                     pq.pop();
                 }
             }
-            
             for(int &freq : temp) {
                 if(freq > 0)
                     pq.push(freq);
             }
-            
             if(pq.empty()) //all processes finished
                 time += temp.size();
             else
                 time += (p+1); //we finished p+1 tasks above in the loop
-            
         }
         
         return time;
