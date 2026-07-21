c89e5e846fc984d9a8c95c1a83ab6b61b4bea724
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-21 22:01:25 +0530

    Time: 0 ms (100%), Space: 10.9 MB (78.12%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -9,6 +9,7 @@ public:
             if(lastSeen[0]!=-1 && lastSeen[1]!=-1  && lastSeen[2]!=-1 ){
                 count += 1 + min({lastSeen[0], lastSeen[1], lastSeen[2]});
                 // adding everything at the left which is min+1;
+                // basically every start upto the minLast seen is valid because all of them will be containing all the characters and 0 based indexing so added 1
             }
         }
         return count;
