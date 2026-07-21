248ce918dd94b88d433c975802e437ed375a064a
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-21 23:56:13 +0530

    Time: 0 ms (100%), Space: 20.7 MB (48.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,8 +5,9 @@ public:
         int farthest =0;
         int jumps =0;
         int currend =0;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n-1;i++){ // we are taking the benefit of the point that we are sure that we can reach to the end so we just calculate the farthes we can go with every jump and only add when we reach to that index
             farthest = max(farthest,nums[i]+i);
+            if(farthest >= n-1) return jumps+1;
             if(i==currend){
                 jumps++;
                 currend = farthest;
