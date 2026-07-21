e70a6c923b0b75efd34a3d2faed85776b0fd0c48
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-21 12:47:43 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,45 @@
 class Solution {
+    int findFirst(vector<int>&nums,int target){
+        int low = 0, high = nums.size()-1;
+        int res = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){ 
+                res = mid;
+                high = mid-1;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+    int findLast(vector<int>&nums,int target){
+        int low = 0, high = nums.size()-1;
+        int res = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target){ 
+                res = mid;
+                low = mid+1;
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return res;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
         int low = 0, high = n-1;
         //first occurence
-        int f=-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target){
-                f=mid;
-                high = mid-1;
-            }
-            else if(nums[mid]>target){
-                high = mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        int l=-1;
-        low = 0; high = n-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid]==target){
-                l=mid;
-                low = mid+1;
-            }
-            else if(nums[mid]<target){
-                low = mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
+        int f = findFirst(nums,target);
+        int l = findLast(nums,target);
         return {f,l};
     }
 };
