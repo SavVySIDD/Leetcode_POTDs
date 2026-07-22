4d631bdc893d4966348847ffe156abaafadc3b5f
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-22 23:22:15 +0530

    Time: 49 ms (27.75%), Space: 49.6 MB (22.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,21 +1,18 @@
 class Solution {
     vector<vector<int>>dp;
+    //unbounded Knapsack
     int solve(vector<int>& coins, int amount, int ind){
-        if(amount==0) return 0;
-        if(ind>=coins.size()) return INT_MAX;
+        if(ind >= coins.size()) return INT_MAX;
+        if(amount ==0) return 0;
+
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int n = coins.size();
         int take = INT_MAX;
-
         if(coins[ind]<=amount){
-            int res = solve(coins,amount-coins[ind],ind); //Here notice the index is not changing
-            if(res!=INT_MAX){
-                take = 1+res;//That is why we keep on using the same coin and 
-            }
+            int res = solve(coins,amount-coins[ind],ind);
+            if(res!=INT_MAX) take = 1+res;
         }
-
         int notTake = solve(coins,amount,ind+1);
-        return dp[ind][amount] =  min(take,notTake);
+        return dp[ind][amount] = min(take,notTake);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
