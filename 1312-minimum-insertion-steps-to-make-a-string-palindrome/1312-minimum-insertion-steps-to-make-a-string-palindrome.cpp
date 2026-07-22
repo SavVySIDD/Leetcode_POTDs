c57f68dbaef64a47473bb7f4063e73a4ac819c50
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-22 17:16:31 +0530

    Time: 38 ms (25.52%), Space: 31.7 MB (15.95%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    vector<vector<int>>dp;
+    int solve(string& s, int i, int j){
+        if(i>j) return 0;
+        if(i==j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j]){
+            return dp[i][j] = solve(s,i+1,j-1);
+        }
+        return dp[i][j] = 1 + min(solve(s,i+1,j), solve(s,i,j-1));
+    }
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        dp.resize(n,vector<int>(n,-1));
+        return solve(s,0,n-1);
+    }
+};
