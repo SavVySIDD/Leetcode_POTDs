db40fa38637e94da9667564eda07157f2d71611c
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-23 15:50:42 +0530

    Time: 0 ms (100%), Space: 11.3 MB (39.81%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,30 +1,54 @@
 class Solution {
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head == NULL || head->next == NULL) {
+
+        if (!head || left == right)
             return head;
-        }
-        
+
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
-        
-        ListNode* prev = dummy;
-        
-        for(int i = 1; i<left; i++) {
-            prev = prev->next;
-        }
-        
-        ListNode* curr = prev->next;
-        
-        for(int i = 1; i<=right-left; i++) {
-            
-            ListNode* temp = prev->next; //0
-            prev->next = curr->next; //1
-            curr->next = curr->next->next; //2
-            prev->next->next = temp; //3
-            
-        }
-    
+
+        // Reach node just before 'left'
+        ListNode* leftPrev = dummy;
+        for (int i = 1; i < left; i++)
+            leftPrev = leftPrev->next;
+
+        // First node of sublist
+        ListNode* subHead = leftPrev->next;
+
+        // Reach 'right' node
+        ListNode* rightNode = subHead;
+        for (int i = left; i < right; i++)
+            rightNode = rightNode->next;
+
+        // Store node after 'right'
+        ListNode* rightNext = rightNode->next;
+
+        // Detach the sublist
+        rightNode->next = nullptr;
+
+        // Reverse the detached sublist
+        ListNode* newHead = reverse(subHead);
+
+        // Reconnect
+        leftPrev->next = newHead;
+        subHead->next = rightNext;
+
         return dummy->next;
     }
 };
