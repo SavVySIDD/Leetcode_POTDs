062f36021b61c5d3635b17928ee9fe5c1451185c
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-24 00:51:47 +0530

    Time: 0 ms (100%), Space: 9.3 MB (50.7%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -3,31 +3,25 @@ public:
     string removeDuplicateLetters(string s) {
         int n = s.length();
         string result;
-        
         vector<bool> taken(26, false); //O(1) space
         vector<int> lastIndex(26); //O(1) space
-        
-        for(int i = 0; i < n; i++) {
-            char ch = s[i];
-            
+
+        for(int i = 0; i < n; i++) { // We got the last index from this
+            char ch = s[i]; 
             lastIndex[ch-'a'] = i;
         }
         
-        
         for(int i = 0; i < n; i++) {
-            
             int idx = s[i] - 'a';
-            
             if(taken[idx] == true) continue;
-            
-            while(result.length() > 0 && s[i] < result.back() && lastIndex[result.back() - 'a'] > i) {
+            while(result.length() > 0 && s[i] < result.back() 
+            && lastIndex[result.back() - 'a'] > i)  // Here we check if the already pushed character is larger than this new character and if yes and it appears later we pop it and then it will later be pushed when coming again.
+            {
                 taken[result.back() - 'a'] = false;
                 result.pop_back();
             }
-            
             result.push_back(s[i]);
             taken[idx] = true;
-            
         }
         
         return result;
