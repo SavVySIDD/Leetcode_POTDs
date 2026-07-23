24e8d05220181e9116df3430a57864ca1fea4f2d
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-07-23 17:41:53 +0530

    Time: 7 ms (45.15%), Space: 19.4 MB (48.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,31 @@
 class Solution {
-    //This function essentially checks for cycle 
-    //visPath tells who was the parent or from what path the current node is coming basically if the node is in the recusrions stack or not
-    bool dfsCheck(int node,vector<int>& vis, vector<int>& visPath,vector<vector<int>>& adj ){
+    bool checkCycle(vector<vector<int>>&adjList,vector<int>&vis,vector<int>&visPath, int node){
         vis[node] =1;
         visPath[node] =1;
-        for(auto it: adj[node]){
+        for(auto&it:adjList[node]){
             if(!vis[it]){
-                if(dfsCheck(it,vis,visPath,adj)) return true;
-            }
-            else if(visPath[it]) return true;
-            // So essentially if visited and and in the recursion stack and again came across so cycle found
+                if(checkCycle(adjList,vis,visPath,it)) return true;
+            }else if(visPath[it]) return true;
         }
-        visPath[node]=0;
+        visPath[node] = 0;
         return false;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto& edge: prerequisites){
-            int u = edge[0] ,v = edge[1];
-            adj[v].push_back(u);
+        int n = numCourses;
+        vector<vector<int>>adjList(n);
+        for(auto&it:prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adjList[v].push_back(u);
         }
-        vector<int> vis(numCourses,0),visPath(numCourses,0);
-        for(int i=0;i<numCourses;i++){
+        vector<int>vis(n,0),visPath(n,0);
+        for(int i=0;i<n;i++){
             if(!vis[i]){
-                if(dfsCheck(i,vis,visPath,adj)) return false;
-                //So essentially this means if cycle found all courses can't be finished
+                if(checkCycle(adjList,vis,visPath,i)) return false;
             }
         }
+        for(auto&it:vis){ if(it==0) return false;}
         return true;
     }
 };
