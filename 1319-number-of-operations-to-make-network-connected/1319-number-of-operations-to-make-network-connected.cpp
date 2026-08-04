97a15de14b2d93de09f8bce9a638c85eebe8b57e
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-05 00:04:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (92.53%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,57 @@
+class Solution {
+    class DisjointSet{
+        vector<int>size,parent;
+    public: 
+        DisjointSet(int n){
+            size.assign(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++){
+                parent[i] = i;
+            }
+        }
+        
+        int findUParent(int node){
+            if(parent[node]==node){
+                return node;
+            }
+            return parent[node] = findUParent(parent[node]);
+        }
+        
+        void unionBySize(int u, int v){
+            int ultPu = findUParent(u);
+            int ultPv = findUParent(v);
+            if(ultPu==ultPv){
+                return;
+            }
+            if(size[ultPu] < size[ultPv]){
+                parent[ultPu] = ultPv;
+                size[ultPv] += size[ultPu];
+            }else{
+                parent[ultPv] = ultPu;
+                size[ultPu] += size[ultPv];
+            }
+        }
+    };
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        
+        int extraEdge=0;
+        for(auto&it:connections){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUParent(u) == ds.findUParent(v)){
+                extraEdge++;
+            }else{
+                ds.unionBySize(u,v);
+            }
+        }
+        
+        int totComponents =0;
+        for(int i=0;i<n;i++){
+            if(ds.findUParent(i)==i) totComponents++;
+        }
+        if(extraEdge>=totComponents-1) return totComponents-1;
+        return -1;
+    }
+};
