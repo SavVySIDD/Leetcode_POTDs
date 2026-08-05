c64fd2926b505d11385fe50afd48b6922a1e04be
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-05 16:03:13 +0530

    Time: 25 ms (23.93%), Space: 28.5 MB (65.19%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -3,18 +3,11 @@ public:
     string removeStars(string s) {
         string res = "";
         int n = s.size();
-        stack<char>st;
-        for(int i=n-1;i>=0;i--){
-            st.push(s[i]);
-        }
-        while(!st.empty()){
-            char c = st.top();
-            if(c=='*'){
+        for(char c : s){
+            if(c == '*')
                 res.pop_back();
-            }else{
-                res+=c;
-            }
-            st.pop();
+            else
+                res.push_back(c);
         }
         return res;
     }
