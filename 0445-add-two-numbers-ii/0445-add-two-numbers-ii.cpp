e14eff2fd15c32b66da0e8f865471d8f4ad46e7b
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-07 16:35:31 +0530

    Time: 3 ms (48.14%), Space: 77 MB (67.23%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -21,57 +21,36 @@ class Solution {
     }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1 = reverse(l1);
-        ListNode* t2 = reverse(l2);
-        bool carry = false;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        int carry = 0;
+
         ListNode* dummy = new ListNode(-1);
         ListNode* tail = dummy;
-        while(t1 && t2){
-            int sum = t1->val + t2->val;
-            if(carry){
-                sum++;
+
+        while(l1 || l2 || carry){
+
+            int sum = carry;
+
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
             }
-            carry = false;
-            if(sum>9){
-                carry = true;
+
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
             }
-            tail->next = new ListNode(sum%10);
+
+            carry = sum / 10;
+
+            tail->next = new ListNode(sum % 10);
             tail = tail->next;
-            t1 = t1->next;
-            t2 = t2->next;
         }
 
-        while(t1){
-            int sum = t1->val;
-            if(carry){
-                sum++;
-            }
-            carry = false;
-            if(sum>9){
-                carry = true;
-            }
-            tail->next = new ListNode(sum%10);
-            tail = tail->next;
-            t1 = t1->next;
-        }
-        while(t2){
-            int sum = t2->val;
-            if(carry){
-                sum++;
-            }
-            carry = false;
-            if(sum>9){
-                carry = true;
-            }
-            tail->next = new ListNode(sum%10);
-            tail = tail->next;
-            t2 = t2->next;
-        }
-        if(carry){
-            tail->next = new ListNode(1);
-            tail = tail->next;
-        }
-        dummy = reverse(dummy->next);
-        return dummy;
+        dummy->next = reverse(dummy->next);
+
+        return dummy->next;
     }
 };
