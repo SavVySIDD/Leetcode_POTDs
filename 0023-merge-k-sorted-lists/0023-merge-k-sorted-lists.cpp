9346af1e26ca2570cd29068a24138eba1abf0114
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-09 23:15:26 +0530

    Time: 1 ms (73.29%), Space: 18.4 MB (82.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,32 +9,30 @@
  * };
  */
 class Solution {
-    ListNode* merge2Lists(ListNode*l1, ListNode*l2){
+    ListNode* merge(ListNode* l1, ListNode*l2){
         if(!l1) return l2;
         if(!l2) return l1;
+
         if(l1->val<=l2->val){
-            l1->next = merge2Lists(l1->next,l2);
+            l1->next = merge(l1->next,l2);
             return l1;
-        }
-        else{
-            l2->next = merge2Lists(l2->next,l1);
+        }else{
+            l2->next = merge(l1,l2->next);
             return l2;
         }
-        return NULL;
     }
 
-    ListNode* splitAndMerge(int start, int end, vector<ListNode*>& lists){
+    ListNode* splitAndmerge(vector<ListNode*>& lists, int start, int end){
         if(start>end) return nullptr;
-        if(start == end) return lists[start];
+        if(start==end) return lists[start];
         int mid = start + (end-start)/2;
-        ListNode* l1 = splitAndMerge(start,mid,lists);
-        ListNode* l2 = splitAndMerge(mid+1,end,lists);
-        return merge2Lists(l2,l1);
+        ListNode* l1 = splitAndmerge(lists,start,mid);
+        ListNode* l2 = splitAndmerge(lists,mid+1,end);
+        return merge(l1,l2);
     }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int k = lists.size();
-        if(k==0) return nullptr;
-        return splitAndMerge(0,k-1,lists);
+        return splitAndmerge(lists,0,k-1);
     }
 };
