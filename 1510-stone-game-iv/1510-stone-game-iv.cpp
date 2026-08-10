a57e07521cefc0f4483bee076045207f575b2e3e
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-10 22:14:29 +0530

    Time: 30 ms (83.21%), Space: 8.8 MB (93.89%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,19 +1,22 @@
 class Solution {
-    vector<int>dp;
-    bool solve(int n){
-        if(n==0) return false;
-        if(dp[n]!=-1) return dp[n];
-        
-        for(int i=1;i*i<=n;i++){
-            //Call for the other Player(Alice,Bob)
-            //Call for BOB if false Bob lost it
-            if(!solve(n-i*i)) return dp[n] =true;
-        }
-        return dp[n] = false;
-    }
 public:
     bool winnerSquareGame(int n) {
-        dp.resize(n+1,-1);
-        return solve(n); //Call for Alice
+        vector<bool> t(n+1, false);
+
+        //Base case . n== 0, return false
+        //i == 0
+        t[0] = false; //base case
+
+        for(int i = 1; i < n+1; i++) {
+
+            for(int k = 1; k*k <= i; k++) {
+                if(t[i - (k*k)] == false) {
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return t[n]; //return solve(n)
     }
 };
