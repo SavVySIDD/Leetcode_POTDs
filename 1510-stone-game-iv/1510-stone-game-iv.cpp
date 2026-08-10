4fc2596902c380deaa4c508490119d863ed160d2
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-10 22:09:00 +0530

    Time: 44 ms (47.57%), Space: 16.9 MB (39.1%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,28 +1,19 @@
 class Solution {
-    vector<vector<int>>dp;
-    //Player -> 0 : Alice, 1 : Bob
-    bool solve(int n, int Player){
-        if(n==0 && Player==1) return true;
-        if(n==0 && Player==0) return false;
-        if(dp[Player][n]!=-1) return dp[Player][n];
-        if(Player == 0){ //Alice
-            bool res = false;
-            for(int i=1;i*i<=n;i++){
-                res |=solve(n-(i*i),1);
-            }
-            return dp[Player][n] = res;
-        }else{ //Bob
-            bool res = true;
-            for(int i=1;i*i<=n;i++){
-                res &=solve(n-(i*i),0);
-            }
-            return dp[Player][n] = res;
+    vector<int>dp;
+    bool solve(int n){
+        if(n==0) return false;
+        if(dp[n]!=-1) return dp[n];
+        
+        for(int i=1;i*i<=n;i++){
+            //Call for the other Player(Alice,Bob)
+            //Call for BOB if false Bob lost it
+            if(!solve(n-i*i)) return dp[n] =true;
         }
-        return dp[Player][n] = false;
+        return dp[n] = false;
     }
 public:
     bool winnerSquareGame(int n) {
-        dp.resize(2,vector<int>(n+1,-1));
-        return solve(n,0);
+        dp.resize(n+1,-1);
+        return solve(n); //Call for Alice
     }
 };
