5995488ee8e7e661d02740de7a3dce918f649715
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-18 19:54:49 +0530

    Time: 6 ms (78.39%), Space: 31.8 MB (64.81%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,49 +1,70 @@
 class Solution {
-private:
-    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& board){
-        vis[r][c] =1;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,-1,0,1};
-        int n = board.size(), m = board[0].size();
-        for(int i=0;i<4;i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-                if(nrow>0 && nrow < n-1 && ncol>0 && ncol <m-1 &&
-                vis[nrow][ncol]!=1 && board[nrow][ncol]==1){
-                    dfs(nrow,ncol,vis,board);
-                }
-            }
-    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        
-        for(int i=0; i<n;i++){ //start & end col
-            if(vis[i][0]!=1 && grid[i][0]==1){
-                dfs(i,0,vis,grid);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
+
+        // Put all boundary land cells into the queue
+        for(int i = 0; i < n; i++) {
+            if(grid[i][0] == 1 && !vis[i][0]) {
+                vis[i][0] = 1;
+                q.push({i, 0});
             }
-            if(vis[i][m-1]!=1 && grid[i][m-1]==1){
-                dfs(i,m-1,vis,grid);
+            if(grid[i][m-1] == 1 && !vis[i][m-1]) {
+                vis[i][m-1] = 1;
+                q.push({i, m-1});
             }
         }
 
-        for(int j=0;j<m;j++){ //start and end row
-            if(vis[0][j]!=1 && grid[0][j]==1){
-                dfs(0,j,vis,grid);
+        for(int j = 0; j < m; j++) {
+            if(grid[0][j] == 1 && !vis[0][j]) {
+                vis[0][j] = 1;
+                q.push({0, j});
             }
-            if(vis[n-1][j]!=1 && grid[n-1][j]==1){
-                dfs(n-1,j,vis,grid);
+            if(grid[n-1][j] == 1 && !vis[n-1][j]) {
+                vis[n-1][j] = 1;
+                q.push({n-1, j});
             }
         }
-        int count =0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]==1){
+
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, -1, 0, 1};
+
+        while(!q.empty()) {
+
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int i = 0; i < 4; i++) {
+
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow >= 0 && nrow < n &&
+                   ncol >= 0 && ncol < m &&
+                   !vis[nrow][ncol] &&
+                   grid[nrow][ncol] == 1) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+
+        // Remaining unvisited land cells are enclaves
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == 1 && !vis[i][j]) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 };
