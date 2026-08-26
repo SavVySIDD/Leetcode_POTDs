4b5900b19766947739479647f488105aa374a953
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-08-26 11:37:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.53%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool isLexicographicallySmall(string s,int left,int sIdx){
+        int tind1 = sIdx, tind2 = left;
+        while(s[tind1]==s[tind2]){
+            tind1++;
+            tind2++;
+        }
+        if(s[tind1]>s[tind2]) return true;
+        return false;
+    }
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int left  = 0;
+        int count = 0;
+        int n     = s.size();
+        int mini  = INT_MAX;
+        int sIdx  = -1;
+        for(int right=0;right<n;right++){
+            if(s[right]-'0' == 1) count++;
+            while(count == k){
+                if(right-left+1 < mini){
+                    mini = right-left+1;
+                    sIdx = left;
+                }else if(right-left+1 == mini && isLexicographicallySmall(s,left,sIdx)){
+                    sIdx = left;
+                }
+                if(s[left]-'0' == 1) count--;
+                left++;
+            }
+        }
+        return sIdx==-1?"":s.substr(sIdx,mini);
+    }
+};
