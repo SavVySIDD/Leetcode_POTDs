f28aefb4ffabd910a9916bef47611569b499ae00
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-09-02 10:51:11 +0530

    Time: 4 ms (31.19%), Space: 15.5 MB (61.22%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    void solve(int n, vector<string>&res,int open,int close,string curr){
+        if(curr.size()==2*n){
+            res.push_back(curr);
+            return;
+        }
+        if(open<n)solve(n,res,open+1,close,curr+'(');
+        if(close<open)solve(n,res,open,close+1,curr+')');
+        return;
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        solve(n,res,0,0,"");
+        return res;
+    }
+};
