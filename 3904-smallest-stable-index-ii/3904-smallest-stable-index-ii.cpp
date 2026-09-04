1563a29ec6b8111f6520d8f10c2d3475ae35e1af
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-09-05 02:01:35 +0530

    Time: 8 ms (70.04%), Space: 210.8 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>pge(n),nse(n);
+        pge[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pge[i] = max(pge[i-1],nums[i]);
+        }
+        nse[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            nse[i] = min(nse[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            int instability = pge[i]-nse[i];
+            if(instability <= k) return i;
+        }
+        return -1;
+    }
+};
