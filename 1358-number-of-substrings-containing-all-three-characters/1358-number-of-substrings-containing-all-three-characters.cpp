f7cd50a058e0250218fef3eb22d7326072096ef5
Siddhan Baranwal <siddhanbaranwal@gmail.com>
2026-09-21 10:56:37 +0530

    Time: 50 ms (5.52%), Space: 17.9 MB (5.06%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int lastSeen[3] = {-1,-1,-1};
+    int countChars(string s, int k){
+        if(k<0) return 0;
+        int n= s.size();
         int count =0;
-        int n = s.size();
-        for(int i=0;i<n;i++){
-            lastSeen[s[i]-'a'] = i;
-            if(lastSeen[0]!=-1 && lastSeen[1]!=-1  && lastSeen[2]!=-1 ){
-                count += 1 + min({lastSeen[0], lastSeen[1], lastSeen[2]});
-                // adding everything at the left which is min+1;
-                // basically every start upto the minLast seen is valid because all of them will be containing all the characters and 0 based indexing so added 1
+        unordered_map<char,int> mp;
+        int left=0;
+        for(int right =0;right<n;right++){
+            mp[s[right]]++;
+            if(mp.size()>k){
+                while(mp.size()>k){
+                mp[s[left]]--;
+                if(mp[s[left]]==0) mp.erase(s[left]);
+                left++;
+                }
             }
+            count+= right-left+1;
         }
         return count;
+    }
+
+    int numberOfSubstrings(string s) {
+        return countChars(s,3)- countChars(s,2);
     }
 };
